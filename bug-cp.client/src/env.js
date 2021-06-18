09fa4516e008e0d9b3f9f4cdd4916ba2546536a3
hannah hayward <hannahhayward97@gmail.com>
2021-06-18 15:07:16 -0600

    initial set-up complete

---
+++ bug-cp.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'hsquared.us.auth0.com'
+export const audience = 'https://hsquared.us.auth0.com/api/v2/'
+export const clientId = 'ciJtopXkpoPcXK7MMHOj83eAeJLfk1XM'
